3dfb990188eec9f30d9e3c8cf576bc8568692c42
Mykhaylo <hrycenkomykhaylo@gmail.com>
2023-10-01 12:23:58 +0300

    commit_0000012-36

--- js/lesson_14.js
+++ js/lesson_14.js
@@ -1,5 +1,5 @@
 'use strict';
-const category = 'toys';
+const category = 'TOYS';
 console.log('https://someurl.com/' + category + '/' + '5'); // 1-й вариант записи через обычные кавычки '
 console.log(`https://someurl.com/${category}/5`); // 2-й вариант записи только через бектики "`"
 const user = 'Ivan';
